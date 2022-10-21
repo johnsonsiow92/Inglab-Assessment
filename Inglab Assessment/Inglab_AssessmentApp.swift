c60480e4fbf45c479ef92f15a3cd9149f534c9c9
Siow Johnson <johnson@thelorry.com>
2022-10-21 14:36:10 +0800

    Initial Commit

---
+++ Inglab Assessment/Inglab_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inglab_AssessmentApp.swift
+//  Inglab Assessment
+//
+//  Created by The Lorry Online on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Inglab_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
